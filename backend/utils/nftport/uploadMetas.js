ffd0884a6ca368a888592ffae110cb440d4e7a42
Alvindra Aditya <alvindra14@gmail.com>
2022-08-10 15:37:40 +0700

    change nft port n more

--- backend/utils/nftport/uploadMetas.js
+++ backend/utils/nftport/uploadMetas.js
@@ -76,7 +76,7 @@ async function main() {
       "external_url": metaData?.external_url,
       "custom_fields": {
         "date": metaData.date,
-        "compiler": "HashLips Art Engine - codeSTACKr Modified"
+        "compiler": "TheBeastLabs"
       }
     }
 
