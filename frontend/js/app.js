777d0e9c8a754d03e7a3a9855230bf5a4e39581c
Alvindra Aditya <alvindra14@gmail.com>
2022-08-19 04:14:14 +0700

    spiner

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -50,7 +50,7 @@ const updateConnectStatus = async () => {
   const onboarding = new MetaMaskOnboarding();
   const onboardButton = document.getElementById("connectWallet");
   const notConnected = document.querySelector('.not-connected');
-  const spinner = document.getElementById("spinner");
+  // const spinner = document.getElementById("spinner");
   if (!MetaMaskOnboarding.isMetaMaskInstalled()) {
     onboardButton.innerText = "Install MetaMask!";
     onboardButton.onclick = () => {
@@ -58,7 +58,7 @@ const updateConnectStatus = async () => {
       onboardButton.disabled = true;
       onboarding.startOnboarding();
       // HIDE SPINNER
-      spinner.classList.add('hidden');
+      // spinner.classList.add('hidden');
       notConnected.classList.remove('hidden');
       notConnected.classList.add('show-not-connected');
     };
@@ -70,13 +70,13 @@ const updateConnectStatus = async () => {
     notConnected.classList.remove('show-not-connected');
     notConnected.classList.add('hidden');
     // SHOW SPINNER
-    spinner.classList.remove('hidden');
+    // spinner.classList.remove('hidden');
     window.contract = new web3.eth.Contract(abi, contractAddress);
     loadInfo();
   } else {
     onboardButton.innerText = "Connect MetaMask!";
-    // HIDE SPINNER
-    spinner.classList.add('hidden');
+    // // HIDE SPINNER
+    // spinner.classList.add('hidden');
     notConnected.classList.remove('hidden');
     notConnected.classList.add('show-not-connected');
     onboardButton.onclick = async () => {
@@ -89,7 +89,7 @@ const updateConnectStatus = async () => {
           notConnected.classList.remove('show-not-connected');
           notConnected.classList.add('hidden');
           // SHOW SPINNER
-          spinner.classList.remove('hidden');
+          // spinner.classList.remove('hidden');
           onboardButton.disabled = true;
           window.address = accts[0];
           accounts = accts;
@@ -166,7 +166,7 @@ async function loadInfo() {
   const actionButton = document.getElementById("actionButton");
   const mintContainer = document.getElementById("mintContainer");
   const mintButton = document.getElementById("mintButton");
-  const spinner = document.getElementById("spinner");
+  // const spinner = document.getElementById("spinner");
 
   let startTime = "";
   if (publicMintActive) {
@@ -231,7 +231,7 @@ async function loadInfo() {
   countdown();
 
   // HIDE SPINNER
-  spinner.classList.add('hidden');
+  // spinner.classList.add('hidden');
 
   // SHOW CARD
   setTimeout(() => {
@@ -319,8 +319,8 @@ function setTotalPrice() {
 async function mint() {
   const mintButton = document.getElementById("mintButton");
   mintButton.disabled = true;
-  const spinner = '<div class="dot-elastic"></div><span>Waiting for transaction...</span>';
-  mintButton.innerHTML = spinner;
+  // const spinner = '<div class="dot-elastic"></div><span>Waiting for transaction...</span>';
+  // mintButton.innerHTML = spinner;
 
   const amount = parseInt(document.getElementById("mintInput").value);
   const value = BigInt(info.deploymentConfig.mintPrice) * BigInt(amount);
